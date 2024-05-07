596a6044a7d140023655556bfb696f55b54741f7
Bang28 <mhdmasruri9@gmail.com>
2024-05-07 09:14:02 +0700

    First Leaflet Map

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-window.alert('It works from the windows alert')
+window.onload = init;
 
-console.log('It works')
+function init() {
+    const mymap = L.map('mapid', {
+        center: [48, 14],
+        zoom: 4,
+        layers: [
+            L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+                attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                ext: 'png'
+            })
+        ]
+    })
+}
