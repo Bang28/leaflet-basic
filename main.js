01346975a5182d1d2ae517a08499ab989d863c18
Bang28 <mhdmasruri9@gmail.com>
2024-05-07 13:25:13 +0700

    Central Leaflet Class Map - overview

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 window.onload = init;
 
 function init() {
-    const mymap = L.map('mapid', {
+
+
+    const mapElement = document.getElementById('mapid')
+    
+    const mymap = L.map(mapElement, {
         center: [48, 14],
-        zoom: 4,
+        zoom: 5,
+        minZoom: 4,
         layers: [
             L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
                 attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -11,4 +16,14 @@ function init() {
             })
         ]
     })
+
+    mymap.on('resize', function(e){
+        console.log('The map has been resized')
+    })
+
+    mymap.on('resize', function(e){
+        mymap.flyTo([0, 0])
+    })
+
+    console.log(mymap.getPanes().tilePane)
 }
