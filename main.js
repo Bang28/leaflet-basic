2f374a63f984c2af5ae55a10e84081ac22257b88
Bang28 <mhdmasruri9@gmail.com>
2024-05-11 17:26:53 +0700

    Extending projections and CRS support in Leaflet with Proj4Leaflet - Part 1

--- main.js
+++ main.js
@@ -23,6 +23,23 @@ function init() {
         // noWrap: true,
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
     });
+
+    var crs = new L.Proj.CRS('EPSG:3006',
+        '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+        {
+            resolutions: [
+                8192, 4096, 2048, 1024, 512, 256, 128,
+                64, 32, 16, 8, 4, 2, 1, 0.5
+            ],
+            origin: [0, 0],
+            bounds: L.bounds([218128.7031, 6126002.9379], [1083427.2970, 7692850.9468])
+        })
+
+    var kartenaBasemap = L.tileLayer('https://api.geosition.com/tile/osm-bright-3006/{z}/{x}/{y}.png', {
+        maxZoom: 14,
+        // minZoom: 0,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>, Imagery &copy; <a href="http://www.kartena.se/">Kartena</a>'
+    })
     
     // initialize the map
     const mymap = L.map(mapElement, {
@@ -34,10 +51,15 @@ function init() {
         easeLinearity: 0.5,
         // worldCopyJump: true, 
         // layers: [openStreetMapStandard], // sett your default map in here
-        layers: [openStreetMapStandard_Basic_V2], // sett your default map in here
+        // layers: [openStreetMapStandard_Basic_V2], // sett your default map in here
+        layers: [kartenaBasemap], // sett your default map in here
         // crs: L.CRS.EPSG3857,
+        crs: crs
 
     });
+
+    mymap.setView([57.704, 11.965], 13);
+
 
     // leaflet basemaps object
     const baseLayers = {
